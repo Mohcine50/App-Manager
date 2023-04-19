d31f439ac9a4b02eab0b7d85a5b8d06567dadc79
shegami <mohcinela1998@gmail.com>
2023-04-19 01:30:06 +0000

    update

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,7 +3,7 @@
 import { SessionProvider, useSession } from "next-auth/react";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <SessionProvider>{children}</SessionProvider>;
+	return <SessionProvider>{children}</SessionProvider>;
 };
 
 export default Providers;
