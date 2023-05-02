1ee40988ef1956b7e2b2af821f1997105cee10f1
shegami <mohcinela1998@gmail.com>
2023-05-02 23:18:00 +0100

    update imports

--- src/app/api/data/[id]/route.ts
+++ src/app/api/data/[id]/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "../../../../../utils/prismaClient";
 
 export async function GET(request: NextRequest, { params }: { params: any }) {
