e0f8b6faec020ac6237715a4a6dde36e56ffda24
shegami <mohcinela1998@gmail.com>
2023-12-11 20:53:10 +0100

    change revalidation for test

--- src/app/components/appsTable.tsx
+++ src/app/components/appsTable.tsx
@@ -33,10 +33,6 @@ const AppsTable = ({ apps }: IProps) => {
     router.refresh();
   };
 
-  useEffect(() => {
-    router.refresh();
-  }, [router]);
-
   return (
     <table className="table-auto w-[100%]">
       <thead>
